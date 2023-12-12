e5c0dab7c816f8676c1ff6c84d11f4aedc56a316
KsQRooW <dimankochetov@yandex.ru>
2023-12-13 01:11:21 +0300

    test(routers): добавлена первая fixture, которая возвращает клиент для тестирования приложения GitLabWH

---
+++ tests/routers/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from src.main import gitlab_wh
+
+
+@pytest.fixture()
+def client() -> TestClient:
+    """Возвращает экземпляр приложения FastAPI для тестирования"""
+    return TestClient(gitlab_wh)
