df62ac587522cda2e4994610edea418aeac6dec3
nparamonov <nikita@nparamonov.ru>
2023-12-17 18:00:06 +0300

    feat(app): добавлен mount StaticFiles

--- src/app/base.py
+++ src/app/base.py
@@ -1,10 +1,16 @@
+from pathlib import Path
 from typing import Any
 
 from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
 
 
 class GitLabWH(FastAPI):
     """Приложение GitLab-WH"""
+
+    CURRENT_PATH = Path(__file__).absolute().parent.parent
+    STATIC_FOLDER_PATH = Path(CURRENT_PATH, "static")
+
     def __init__(self, *, main_router: APIRouter, **kwargs: Any):
         """Конструктор
 
@@ -12,3 +18,4 @@ class GitLabWH(FastAPI):
         """
         super().__init__(**kwargs)
         self.include_router(main_router)
+        self.mount("/static", StaticFiles(directory=self.STATIC_FOLDER_PATH), name="static")
