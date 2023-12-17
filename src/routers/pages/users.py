38a92c78633ce9bcc7223626aa4dc44cba44459c
nparamonov <nikita@nparamonov.ru>
2023-12-18 00:21:30 +0300

    feat(routers): добавлена первичная реализация для метода авторизации

--- src/routers/pages/users.py
+++ src/routers/pages/users.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 users_router = APIRouter(prefix="/users", tags=["users"])
+# TODO должно быть вынесено в отдельный класс + dependency
+templates = Jinja2Templates(directory="src/templates")
 
 
-@users_router.get("/sign_in", summary="Страница входа")
-async def sign_in() -> None:
+@users_router.get("/sign_in", response_class=HTMLResponse, summary="Страница входа")
+async def sign_in(request: Request) -> Any:
     """Страница входа в админ панель"""
-    return
+    return templates.TemplateResponse("pages/users/sign_in.html.j2", {"request": request})
