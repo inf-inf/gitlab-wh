76204d5db4a887170223bbb3f9a817b6380af8f1
nparamonov <nikita@nparamonov.ru>
2023-12-19 01:22:23 +0300

    refactor(routers): метод /sign_in использует CommonTemplateResponseGenerator для генерации страницы

--- src/routers/pages/users.py
+++ src/routers/pages/users.py
@@ -1,15 +1,17 @@
-from typing import Any
+from typing import Annotated
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Response
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
+from src.app.templates import CommonTemplateResponseGenerator
+from src.dependencies.templates import get_common_trg_prefill_path
 
 users_router = APIRouter(prefix="/users", tags=["users"])
-# TODO должно быть вынесено в отдельный класс + dependency
-templates = Jinja2Templates(directory="src/templates")
+
+get_trg_dep = Annotated[CommonTemplateResponseGenerator, Depends(get_common_trg_prefill_path("pages/users"))]
 
 
 @users_router.get("/sign_in", response_class=HTMLResponse, summary="Страница входа")
-async def sign_in(request: Request) -> Any:
+async def sign_in(get_trg: get_trg_dep) -> Response:
     """Страница входа в админ панель"""
-    return templates.TemplateResponse("pages/users/sign_in.html.j2", {"request": request})
+    return get_trg.generate_response("sign_in.html.j2")
