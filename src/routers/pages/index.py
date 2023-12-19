f783c972962791f36a69cbfc9478e196f4c701c9
nparamonov <nikita@nparamonov.ru>
2023-12-19 22:38:41 +0300

    feat(routers): добавлен редирект /favicon.ico на расположение фавикона

--- src/routers/pages/index.py
+++ src/routers/pages/index.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import RedirectResponse
 
 index_router = APIRouter(tags=["index"])
 
@@ -7,3 +8,8 @@ index_router = APIRouter(tags=["index"])
 async def index() -> None:
     """Главная страница"""
     return
+
+@index_router.get("/favicon.ico", summary="Редирект фавикон", response_class=RedirectResponse)
+async def redirect_favicon() -> RedirectResponse:
+    """В случае если на странице не указан адрес фавикона, средиректить браузер"""
+    return RedirectResponse(url="/static/img/favicon.png", status_code=status.HTTP_301_MOVED_PERMANENTLY)
