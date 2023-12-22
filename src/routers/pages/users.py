12125e705e9f507ed35a8d05066b5e4dc9bcdb99
nparamonov <nikita@nparamonov.ru>
2023-12-23 00:53:10 +0300

    feat(routers): добавлен POST роут для обработки авторизации

--- src/routers/pages/users.py
+++ src/routers/pages/users.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Query, Response
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, Form, Query, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 from src.app.templates import CommonTemplateResponseGenerator
 from src.dependencies.templates import get_common_trg_prefill_path
@@ -12,12 +12,27 @@ GetTRGDep = Annotated[CommonTemplateResponseGenerator, Depends(get_common_trg_pr
 
 
 @users_router.get("/sign_in", response_class=HTMLResponse, summary="Страница входа")
-async def sign_in(get_trg: GetTRGDep,
-                  redirect: str = Query("/", description=("Страница, на которую произойдет редирект в случае "
-                                                          "успешной авторизации")),
-                  ) -> Response:
+async def get_sign_in(get_trg: GetTRGDep,
+                      redirect: str = Query("/", description=("Страница, на которую произойдет редирект в случае "
+                                                              "успешной авторизации")),
+                      ) -> Response:
     """Страница входа в админ панель"""
     context = {
           "redirect": redirect,
     }
     return get_trg.generate_response("sign_in.html.j2", context)
+
+@users_router.post("/sign_in", summary="Обработка авторизации")
+async def post_sign_in(get_trg: GetTRGDep,
+                       username: str = Form(min_length=1, max_length=20, description="Имя пользователя"),
+                       password: str = Form(min_length=1, max_length=20, description="Пароль"),  # noqa: ARG001
+                       redirect: str = Form("/", description=("Страница, на которую произойдет редирект в случае "
+                                                              "успешной авторизации")),
+                       ) -> Response:
+      """Авторизация, проверка логина и пароля"""
+      context = {
+            "username": username,
+            "redirect": redirect,
+            "alert": {"level": "error", "msg": "Неверный логин или пароль"},
+      }
+      return get_trg.generate_response("sign_in.html.j2", context)
