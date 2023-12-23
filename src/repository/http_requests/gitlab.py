416818ac876503f7fc06dc4bf73e2f86731c270a
KsQRooW <dimankochetov@yandex.ru>
2023-12-23 15:29:43 +0300

    refactor(repository): urls GitLab API теперь находятся в отдельных переменных

--- src/repository/http_requests/gitlab.py
+++ src/repository/http_requests/gitlab.py
@@ -1,15 +1,13 @@
-from typing import ClassVar
+from http import HTTPStatus
 
 from .base import BaseHTTP
 
 
 class GitLabHTTP(BaseHTTP):
     """Запросы в GitLab API"""
-    URLS: ClassVar[dict[str, str]] = {
-        "ping": "/api/v4/projects",
-    }
+    PING = "/api/v4/projects"
 
     async def check(self) -> bool:
         """Проверка доступности GitLab API"""
-        response = await self._get(self.URLS["ping"])
-        return response.status_code == 200 and isinstance(response.data, list)
+        response = await self._get(self.PING)
+        return response.status_code == HTTPStatus.OK and isinstance(response.data, list)
