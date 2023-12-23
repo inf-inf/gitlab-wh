7d3188762db16e5e0190c2ba23778c835cf03f1b
KsQRooW <dimankochetov@yandex.ru>
2023-12-23 16:14:30 +0300

    fix(repository): исправлено наименование атрибута у FakeResponse

--- src/repository/http_requests/fake_http.py
+++ src/repository/http_requests/fake_http.py
@@ -10,19 +10,19 @@ class FakeResponse(Generic[T]):
     """Mock Response object"""
     def __init__(self,
                  data: T | None = None,
-                 status_code: int = 200,
+                 status: int = 200,
                  headers: dict[str, str] | None = None,
                  ) -> None:
         """Конструктор
 
         Args:
             data: данные, которые вернутся в методе .json()
-            status_code: статус код, который вернется при вызове атрибута .status
+            status: статус код, который вернется при вызове атрибута .status
             headers: HTTP заголовки, которые вернутся при вызове атрибута .headers
         """
         self.data = data
         self.headers = headers or {}
-        self.status_code = status_code
+        self.status = status
 
     async def __aenter__(self) -> "FakeResponse[T]":
         """Mock реализация __aenter__"""
@@ -51,7 +51,7 @@ class FakeClientSession(ClientSession):
 
         Args:
             data: данные, которые вернутся в методе FakeResponse.json()
-            status: статус код, который вернется при вызове атрибута FakeResponse.status
+            status_code: статус код, который вернется при вызове атрибута FakeResponse.status
             headers: HTTP заголовки, которые вернутся при вызове атрибута FakeResponse.headers
         """
         self._fake_response = FakeResponse(data, status_code, headers)
