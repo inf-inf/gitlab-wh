9b950b457c35a03f3f68c6063b1489ee2abb25ad
nparamonov <nikita@nparamonov.ru>
2024-01-27 00:05:15 +0300

    feat(routers): добавлен рендеринг шаблона для главной страницы

--- src/routers/pages/index.py
+++ src/routers/pages/index.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Response, status
 from fastapi.responses import RedirectResponse
+
+from src.app.templates import CommonTemplateResponseGenerator
+from src.dependencies.templates import get_common_trg_prefill_path
 
 index_router = APIRouter(tags=["pages.index"])
 
+GetTRGDep = Annotated[CommonTemplateResponseGenerator, Depends(get_common_trg_prefill_path("pages/common"))]
+
 
 @index_router.get("/", summary="Главная страница")
-async def index() -> None:
+async def index(get_trg: GetTRGDep) -> Response:
     """Главная страница"""
-    return
+    return get_trg.generate_response("index.html.j2")
 
 @index_router.get("/favicon.ico", summary="Редирект фавикон", response_class=RedirectResponse)
 async def redirect_favicon() -> RedirectResponse:
