186b02104aa7e69118edac6d7fe951531734a839
KsQRooW <dimankochetov@yandex.ru>
2024-01-30 21:40:05 +0300

    fix(routers): удален случайно добавленный route /debug

--- src/routers/api/service.py
+++ src/routers/api/service.py
@@ -11,19 +11,3 @@ async def ping() -> PlainTextResponse:
     Должен отвечать `pong` при доступности и работоспособности приложения
     """
     return PlainTextResponse("pong")
-
-
-from aiohttp import ClientSession
-
-from src.repository.http_requests.gitlab import GitLabHTTPv4
-
-
-@service_router.get("/debug", summary="Проверка доступности API")
-async def debug() -> PlainTextResponse:
-    """Метод проверки доступности API
-
-    Должен отвечать `pong` при доступности и работоспособности приложения
-    """
-    async with ClientSession("http://localhost", headers={"PRIVATE-TOKEN": "test-token-123"}) as session:
-        gitlab_http = GitLabHTTPv4(session)
-        return await gitlab_http.create_group("cool", "cool")
